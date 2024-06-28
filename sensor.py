a0cd90bfaed11700efe5d34b0961ffe932ede313
jadot <benjaminpage.acer@gmail.com>
2024-06-28 20:58:38 +0100

    Changes to support contact sensor.

--- sensor.py
+++ sensor.py
@@ -1,22 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
-SENSOR_PIN = 2  # GPIO 2
+SENSOR_PIN = 14  # GPIO 14
 
 class Sensor:
     """
         LDR control code.
     """
 
-    def __init__(self):
-        """Initialize sensor module"""
+    sensor_pin = SENSOR_PIN
 
-        self.sensor_pin = SENSOR_PIN
+    def __init__(self, sensor_pin = None):
+        """Initialize sensor module"""
+    
+        if sensor_pin != None:
+            sensor_pin = sensor_pin
 
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.sensor_pin, GPIO.IN)
+        GPIO.setup(self.sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def get_state(self):
-        return not GPIO.input(self.sensor_pin)
+        return GPIO.input(self.sensor_pin)
 
